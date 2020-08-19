57fa8156beea0116e75a88e3a5076e7637708f47
Albert Volkman <albert.volkman@gmail.com>
2020-08-19 16:33:57 -0400

    Simplify menu

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -98,3 +98,19 @@ document.querySelectorAll('a[href^="#"]').forEach(function(anchor) {
     });
   });
 });
+
+$('.dropdown-menu a.dropdown-toggle').on('click', function(e) {
+  if (!$(this).next().hasClass('show')) {
+    $(this).parents('.dropdown-menu').first().find('.show').removeClass('show');
+  }
+  var $subMenu = $(this).next('.dropdown-menu');
+  $subMenu.toggleClass('show');
+
+
+  $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function(e) {
+    $('.dropdown-submenu .show').removeClass('show');
+  });
+
+
+  return false;
+});
